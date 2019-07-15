c8ed1dbbc68a4e0c8d89e53d45f25b5bc2cb0fbf
Tony <tonydangelo123@gmail.com>
2019-07-15 22:05:30 +0200

    add morgan and debug

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const proxy = require('express-http-proxy')
+const morgan = require('morgan')
 const express = require('express')
+const debug = require('debug')
 const app = express()
+const dd = debug('express-redirect-service')
 
 if (!process.env.PROXY_URI) {
     console.error('The PROXY_URI env must be specified!')
@@ -8,12 +11,16 @@ if (!process.env.PROXY_URI) {
 }
 
 function proxyReqPathResolver(req) {
-    return `${process.env.PREFIX}${req.url}${process.env.SUFFIX}`
+    const url = `${process.env.PREFIX}${req.url}${process.env.SUFFIX}`
+    dd('url %s', url)
+    return url
 }
 
 const proxyMiddleware = proxy(process.env.PROXY_URI, {
     proxyReqPathResolver
 })
+
+app.use(morgan('combined'))
 
 app.get('/*', proxyMiddleware)
 
